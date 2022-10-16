67d686be408d3038762d255c584b0263213fb800
Toby16 <bolajibaasit14@gmail.com>
2022-10-16 01:04:48 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry
+ * Return: Always 0
+ * I'm a beginner in C programming
+ * I'm just playing around with code
+*/
 
 int main(){
     printf("Enter char value: ");
